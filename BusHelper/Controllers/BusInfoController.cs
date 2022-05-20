c8239ae6272e55416ab09c001c070c0d1dc6a05c
Liu-Steve <73787075+Liu-Steve@users.noreply.github.com>
2022-05-21 03:38:01 +0800

    the bus's position is able to update
    post.py is the script to simulate.
    Just run it.

---
+++ BusHelper/Controllers/BusInfoController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace BusHelper.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class BusInfoController : ControllerBase
+{
+    private static List<Bus> buses = new List<Bus>();
+
+    private readonly ILogger<BusInfoController> _logger;
+
+    public BusInfoController(ILogger<BusInfoController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public IActionResult AddBus(JObject obj)
+    {
+        double? newX = (double?)obj["x"];
+        double? newY = (double?)obj["y"]; 
+        if(newX == null)
+            return BadRequest("Need x in post");
+        if(newY == null)
+            return BadRequest("Need y in post");
+        Bus newBus = new Bus(){
+            X = (double)newX,
+            Y = (double)newY
+        };
+        buses.Add(newBus);
+        return Ok(newBus.BusID);
+    }
+
+    [HttpPost]
+    public IActionResult UpdateBus(JObject obj)
+    {
+        string? busID = (string?)obj["bus_id"];
+        double? newX = (double?)obj["x"];
+        double? newY = (double?)obj["y"]; 
+        if(busID == null)
+            return BadRequest("Need bus_id in Post");
+        if(newX == null)
+            return BadRequest("Need x in post");
+        if(newY == null)
+            return BadRequest("Need y in post");
+        Bus? updateBus = buses.Find(bus => bus.BusID == busID);
+        if(updateBus == null)
+            return BadRequest("No bus matched is found");
+        updateBus.X = (double)newX;
+        updateBus.Y = (double)newY;
+        return Ok();
+    }
+
+    [HttpGet]
+    public IEnumerable<Bus> GetBusPos()
+    {
+        return buses.ToArray();
+    }
+}
