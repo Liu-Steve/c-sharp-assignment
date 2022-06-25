e31fdfdcb3c9992238b9b82efe10e721bd90a597
Liu-Steve <steve_liu@whu.edu.cn>
2022-06-25 16:18:22 +0800

    upload file

--- BusHelper/Controllers/BusInfoController.cs
+++ BusHelper/Controllers/BusInfoController.cs
@@ -9,6 +9,11 @@ using BusHelper.Models;
 using BusHelper.Service;
 using Microsoft.AspNetCore.Authorization;
 using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.IdentityModel.Tokens;
+using System.Web;
 
 namespace BusHelper.Controllers;
 
@@ -112,9 +117,9 @@ public class BusInfoController : ControllerBase
     public void PostRealTimeData(RealTimeRecord realTimeRecord)
     {
         //调用API获取结果，解析json写入
-        JObject json=(JObject)JsonConvert.DeserializeObject
-            (DriverBehaviorAnalysis.driver_behavior("img/"+realTimeRecord.RealPic));
-        DriverBehaviorAnalysis.parseJson(realTimeRecord,json);
+        JObject json = (JObject)JsonConvert.DeserializeObject
+            (DriverBehaviorAnalysis.driver_behavior("img/" + realTimeRecord.RealPic));
+        DriverBehaviorAnalysis.parseJson(realTimeRecord, json);
         RealTimeService.addRealTime(realTimeRecord);
     }
 
@@ -141,5 +146,37 @@ public class BusInfoController : ControllerBase
     {
         ArrayList list = RealTimeService.getRealTime(busId);
         return Ok(JsonConvert.SerializeObject(list));
+    }
+
+    [HttpPost, DisableRequestSizeLimit]
+    [Authorize]
+    public async Task<IActionResult> Upload()
+    {
+        try
+        {
+            var formCollection = await Request.ReadFormAsync();
+            var file = formCollection.Files.First();
+            var folderName = Path.Combine("img");
+            var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+            if (file.Length > 0)
+            {
+                var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                var fullPath = Path.Combine(pathToSave, fileName);
+                var dbPath = Path.Combine(folderName, fileName);
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                return Ok(new { dbPath });
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal server error: {ex}");
+        }
     }
 }
