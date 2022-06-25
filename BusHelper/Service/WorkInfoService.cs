9e57a62b0718d3e739a5d6b692bca458c8a83413
bbg <2503668406@qq.com>
2022-06-25 16:57:49 +0800

    实现司机打卡

--- BusHelper/Service/WorkInfoService.cs
+++ BusHelper/Service/WorkInfoService.cs
@@ -7,12 +7,14 @@ namespace BusHelper.Service;
 //道路信息
 public class WorkInfoService
 {
-    //司机打卡
-    public List<Road> getAllRoads(){
+    //司机打卡，插入上班信息表，创建累计风险指标表
+    public static void addWorkInfo(WorkInfo workInfo){
         using (var context = new BusContext(new DbContextOptions<BusContext>()))
         {
-            var query= context.Roads;
-            return query.ToList();
+            workInfo.DangerRecord.WorkInfoId=workInfo.WorkId;
+            context.WorkInfos.Add(workInfo);
+            context.DangerRecords.Add(workInfo.DangerRecord);
+            context.SaveChanges();
         }
     }
 }
