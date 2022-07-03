e7fd54941152af9af6b0ac91c505183eb6507d6d
Liu-Steve <steve_liu@whu.edu.cn>
2022-07-03 16:36:18 +0800

    删除部分多余代码

--- BusTest/AuthTest1.cs
+++ BusTest/AuthTest1.cs
@@ -9,22 +9,10 @@ namespace BusTest;
 
 public class AuthTest1
 {
-    private Mock<IConfigurationSection> confSection;
     private AuthService service;
 
     public AuthTest1()
     {
-        confSection = new Mock<IConfigurationSection>();
-        //实际上线需更换SecretKey
-        confSection.SetupGet(_ => _["SecretKey"]).Returns("b037b073ceb0c08fb3bce5f9c740d2a9a867696a");
-        confSection.SetupGet(_ => _["Issuer"]).Returns("bus-safengine");
-        confSection.SetupGet(_ => _["JWT:Expires"]).Returns("10");
-        confSection.SetupGet(_ => _["Audience"]).Returns("admin");
-
-        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-        string path = System.IO.Directory.GetCurrentDirectory();
-
         var configuration = new ConfigurationBuilder()
             .AddJsonFile($"./appsettings.json")
             .Build();
