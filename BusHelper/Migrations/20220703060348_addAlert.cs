2f34f717587bdcf094a241b87762cdf94e39d624
Liu-Steve <steve_liu@whu.edu.cn>
2022-07-03 14:04:43 +0800

    ef update

---
+++ BusHelper/Migrations/20220703060348_addAlert.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusHelper.Migrations
+{
+    public partial class addAlert : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alert",
+                columns: table => new
+                {
+                    AlertId = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Level = table.Column<int>(type: "int", nullable: false),
+                    Content = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Time = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    IsRead = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    WorkInfoId = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alert", x => x.AlertId);
+                    table.ForeignKey(
+                        name: "FK_Alert_WorkInfos_WorkInfoId",
+                        column: x => x.WorkInfoId,
+                        principalTable: "WorkInfos",
+                        principalColumn: "WorkId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alert_WorkInfoId",
+                table: "Alert",
+                column: "WorkInfoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alert");
+        }
+    }
+}
