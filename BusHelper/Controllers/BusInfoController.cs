ecdbc8e38b284af56fed20d3a4e92382aa244287
Liu-Steve <steve_liu@whu.edu.cn>
2022-07-06 20:45:08 +0800

    add ssl; add mp3

--- BusHelper/Controllers/BusInfoController.cs
+++ BusHelper/Controllers/BusInfoController.cs
@@ -143,7 +143,14 @@ public class BusInfoController : ControllerBase
             var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
             if (file.Length > 0)
             {
-                var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                var header = ContentDispositionHeaderValue.Parse(file.ContentDisposition);
+                var fileName = header.FileName.Trim('"');
+                string ext = Path.GetExtension(fileName);
+                if(ext == ".MP3")
+                {
+                    folderName = Path.Combine("audio");
+                    pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                }
                 var fullPath = Path.Combine(pathToSave, fileName);
                 var dbPath = Path.Combine(folderName, fileName);
                 using (var stream = new FileStream(fullPath, FileMode.Create))
---
+++ BusHelper/Service/FileService.cs
@@ -0,0 +1,15 @@
+namespace BusHelper.Service;
+
+public class FileService
+{
+    private static string[] imgExts = 
+        {".jpg", ".jpeg", ".png", ".JPG", ".JPEG", ".PNG"};
+    
+    private static string[] audioExts = 
+        {".mp3", ".MP3", ".wav", ".WAV"};
+
+    public static int SaveFile(HttpRequest request)
+    {
+        return 0;
+    }
+}
